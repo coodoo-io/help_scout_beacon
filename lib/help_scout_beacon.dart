79bcc651c70ad9d4ab70f5dc60ab69bc40e2ae63
Jan Marsh <1588293+JPM84@users.noreply.github.com>
2024-03-05 16:45:09 +0100

    fix: android release build

--- lib/help_scout_beacon.dart
+++ lib/help_scout_beacon.dart
@@ -2,9 +2,11 @@ import 'package:help_scout_beacon/help_scout_beacon_api.g.dart';
 
 /// Flutter plugin to talk to the Help Scout iOS/Android Beacon SDK
 class HelpScoutBeacon {
+  final api = HelpScoutBeaconApi();
+
   /// Signs in with a Beacon user. This gives Beacon access to the user’s name, email address, and signature.
   Future<void> identify({required HSBeaconUser beaconUser}) async {
-    await HelpScoutBeaconApi().identify(beaconUser: beaconUser);
+    await api.identify(beaconUser: beaconUser);
   }
 
   /// Opens the Beacon SDK from a specific view controller. The Beacon view controller will be presented as a modal.
@@ -13,12 +15,11 @@ class HelpScoutBeacon {
     HSBeaconRoute route = HSBeaconRoute.ask,
     String? parameter,
   }) async {
-    await HelpScoutBeaconApi()
-        .open(settings: settings, route: route, parameter: parameter);
+    await api.open(settings: settings, route: route, parameter: parameter);
   }
 
   /// Logs the current Beacon user out and clears out their information from local storage.
   Future<void> clear() async {
-    await HelpScoutBeaconApi().clear();
+    await api.clear();
   }
 }
